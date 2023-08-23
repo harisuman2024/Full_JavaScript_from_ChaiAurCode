24a21967a43cf303f5bb9bda3be319e0d70da806
HARI SUMAN <95356493+harisuman2024@users.noreply.github.com>
2023-08-23 18:03:40 +0000

    ArrayMethod

---
+++ arrayMethod.js
@@ -0,0 +1,123 @@
+// pop array method
+
+
+/*
+let array = [1, 2, 3, 4, 5];
+let poppedElement = array.pop(); // Removes and returns 5
+console.log(array); // Output: [1, 2, 3, 4]
+console.log(poppedElement); // Output: 5
+*/
+
+// let array = [1, 2, 3, 4, 5];
+// let hari = array.pop(); // Removes and returns 5
+// console.log(array); // Output: [1, 2, 3, 4]
+// console.log(hari); // Output: 5
+
+// push array method 
+
+/* let array = [1, 2, 3];
+array.push(4); // Adds the element 4 to the end of the array
+console.log(array); // Output: [1, 2, 3, 4]
+ */
+
+/* let array = [1, 2, 3];
+array.push(4, 5, 6); // Adds elements 4, 5, and 6 to the end of the array
+console.log(array); // Output: [1, 2, 3, 4, 5, 6] */
+
+
+/* 
+let array = [1, 2, 3];
+let returnHari = array.push(4); // Adds 4 and returns the new length (4)
+console.log(array); // Output: [1, 2, 3, 4]
+console.log(returnHari); // Output: 4 */
+
+
+// Shift Array Method 
+
+/* const fruits = ['apple', 'banana', 'orange'];
+
+const shiftedFruit = fruits.shift();
+
+console.log(shiftedFruit); // Output: 'apple'
+console.log(fruits); // Output: ['banana', 'orange']
+  */
+
+
+// unshift() array mehtod 
+
+/* const fruits = ['banana', 'orange'];
+
+fruits.unshift('apple', 'grape');
+
+console.log(fruits); // Output: ['apple', 'grape', 'banana', 'orange'] */
+
+
+// concat() array method 
+
+/* const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const concatenatedArray = array1.concat(array2);
+
+console.log(concatenatedArray); // Output: [1, 2, 3, 4, 5, 6] */
+
+
+// slice array method 
+/* 
+const originalArray = [1, 2, 3, 4, 5];
+const slicedArray = originalArray.slice(1, 4);
+
+console.log(slicedArray); // Output: [2, 3, 4] */
+
+// splice array method in javascript 
+
+
+/* const originalArray = [1, 2, 3, 4, 5];
+const removedAndAdded = originalArray.splice(1, 3, 8, 9, 10); // Remove 3 elements and add 3 elements starting from index 1
+
+console.log(originalArray); // Output: [1, 8, 9, 10, 5]
+console.log(removedAndAdded); // Output: [2, 3, 4]
+ */
+
+
+// Join Array method in javascript 
+
+/* const array = ['apple', 'banana', 'grape'];
+const joinedString = array.join(', ');
+
+console.log(joinedString); // Output: "apple, banana, grape" */
+
+
+
+// reverse Array method in javascript   
+
+/* const array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output: [5, 4, 3, 2, 1] */
+
+
+
+// tostring() Array method in javascript 
+
+/* const array = [1, 2, 3, 4, 5];
+const arrayAsString = array.toString();
+
+console.log(arrayAsString); // Output: "1,2,3,4,5"
+  */
+
+
+
+// isArray() Array method in javascript  
+
+const array = [1, 2, 3];
+const notAnArray = 'Hello, World!';
+
+console.log(Array.isArray(array)); // Output: true
+console.log(Array.isArray(notAnArray)); // Output: false
+
+
+
+ 
+
+
+
+
